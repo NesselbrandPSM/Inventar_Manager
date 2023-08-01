e060b1cc755a10ebf1475712ddb769c008aa3c60
l.schmidt <l.schmidt@perfectus-solutions.de>
2023-08-01 13:15:38 +0200

    more cleanup

--- src/Main/utility/Utils.java
+++ src/Main/utility/Utils.java
@@ -1,33 +1,9 @@
 package Main.utility;
 
-import SQL.util.TableAttributes;
-
 import java.sql.ResultSet;
-import java.util.ArrayList;
 
 public class Utils {
     public static String[][] convertResultSetTo2DArray(ResultSet res) {
-        ArrayList<ArrayList<String>> table = new ArrayList<>();
-        try {
-            int i = 0;
-            while (res.next()) {
-                table.add(new ArrayList<>());
-                for (TableAttributes attr : TableAttributes.values()) {
-                    table.get(i).add(String.valueOf(res.getObject(attr.toString())));
-                }
-                i++;
-            }
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-        String[][] retTable = new String[table.size()][];
-        for (
-                int i = 0; i < table.size(); i++) {
-            retTable[i] = new String[table.get(i).size()];
-            for (int j = 0; j < table.get(i).size(); j++) {
-                retTable[i][j] = table.get(i).get(j);
-            }
-        }
-        return retTable;
+        return null;
     }
 }
