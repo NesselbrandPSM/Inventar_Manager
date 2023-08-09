5af72962ca0952b1e1c94d09e372c9f1cd3920f1
BuildTools <unconfigured@null.spigotmc.org>
2023-08-09 15:46:52 +0200

    test2

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -19,6 +19,7 @@ public class Main {
     }
 
     private void init(){
+        //Test
         connector = new SQLConnector();
         sqlSequenzStatements = new SQLSequenzStatements(connector);
         sqlStatements = new SQLStatements(connector);
