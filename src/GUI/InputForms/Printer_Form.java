4ea63189711c2d0e8f8c6e2a08b9000cc2731a0d
BuildTools <unconfigured@null.spigotmc.org>
2023-08-13 20:10:52 +0200

    added printer form

--- src/GUI/InputForms/Printer_Form.java
+++ src/GUI/InputForms/Printer_Form.java
@@ -1,11 +1,42 @@
 package GUI.InputForms;
 
+import GUI.util.ComboBoxItem;
+import SQL.SQLConnector;
+import SQL.Statements.SQLSelectStatements;
+
 import javax.swing.*;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class Printer_Form {
     private JPanel printerPanel;
+    private JTextField currentStatus;
+    private JComboBox companys;
+    private JTextField manufacturer;
+    private JTextField modell;
+    private JTextField s_number;
+    private JTextField eq_number;
+    private JTextField ip;
+    private JTextField purchaseDate;
+    private JTextField purchaseNumber;
+    private JTextField purchasePrice;
+    private JTextField warranty;
+
+    private SQLSelectStatements sqlSelectStatements;
 
     public Printer_Form() {
+        sqlSelectStatements = new SQLSelectStatements(new SQLConnector());
+
+        companys.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                companys.removeAllItems();
+                String[] companysArr = sqlSelectStatements.getAllCompanys()[0];
+                for (String s : companysArr) {
+                    companys.addItem(new ComboBoxItem(s));
+                }
+            }
+        });
     }
 
     public JPanel getPrinterPanel() {
