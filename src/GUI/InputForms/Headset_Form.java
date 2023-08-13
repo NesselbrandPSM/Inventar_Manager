282d5cbf3f63b121b6bd956b67aa37615f8deb7a
BuildTools <unconfigured@null.spigotmc.org>
2023-08-13 20:20:57 +0200

    headset form added

--- src/GUI/InputForms/Headset_Form.java
+++ src/GUI/InputForms/Headset_Form.java
@@ -1,11 +1,40 @@
 package GUI.InputForms;
 
+import GUI.util.ComboBoxItem;
+import SQL.SQLConnector;
+import SQL.Statements.SQLSelectStatements;
+
 import javax.swing.*;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class Headset_Form {
     private JPanel headsetPanel;
+    private JTextField currentStatus;
+    private JComboBox companys;
+    private JTextField manufacturer;
+    private JTextField modell;
+    private JTextField s_number;
+    private JTextField purchaseDate;
+    private JTextField purchaseNumber;
+    private JTextField purchasePrice;
+    private JTextField warranty;
+
+    private SQLSelectStatements sqlSelectStatements;
 
     public Headset_Form() {
+        sqlSelectStatements = new SQLSelectStatements(new SQLConnector());
+
+        companys.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                companys.removeAllItems();
+                String[] companysArr = sqlSelectStatements.getAllCompanys()[0];
+                for (String s : companysArr) {
+                    companys.addItem(new ComboBoxItem(s));
+                }
+            }
+        });
     }
 
     public JPanel getHeadsetPanel() {
