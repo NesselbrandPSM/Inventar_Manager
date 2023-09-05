e8d4c44d305ea771c024ffb392ccccdc1e4617f7
l.schmidt <l.schmidt@perfectus-solutions.de>
2023-09-05 13:37:18 +0200

    cleanup

--- src/GUI/MainInputGui.java
+++ src/GUI/MainInputGui.java
@@ -67,7 +67,6 @@ public class MainInputGui {
         MonitorCheckBox.addActionListener(listener);
         TelephoneCheckBox.addActionListener(listener);
 
-
         einfuegenButton.addActionListener(e -> inputEntry());
     }
 
